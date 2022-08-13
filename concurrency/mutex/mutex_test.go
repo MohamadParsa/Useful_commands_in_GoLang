a58ca5ecc557a210d1d879301063278ab3638753
MohamadParsa <MohamadParsa@gmail.com>
2022-08-14 02:23:15 +0430

    .

--- concurrency/mutex/mutex_test.go
+++ concurrency/mutex/mutex_test.go
@@ -8,5 +8,6 @@ func TestCoinToss(t *testing.T) {
 	// to check data race in test step we can use "go test -race ." to check test method with data race condition.
 	// we get "ok" with "go test ." but we get "fail" with "go test -race ."
 	coinTossGameWithRace()
+
 	coinTossGameWithoutRace()
 }
